811be0f32dd06366044e79d2675b88a7da2b95be
Unfunnyisfunny <christian.dociu@gmail.com>
2025-09-20 20:51:03 +0200

    keyword search now based on defined portfolios

--- financeWebApp/interface/management/commands/backend/fetch_data.py
+++ financeWebApp/interface/management/commands/backend/fetch_data.py
@@ -3,6 +3,10 @@ import json
 from newsapi import NewsApiClient
 import time
 import os
+import sys
+sys.path.append('....')
+from ....models import Portfolio
+import string
 dirname = os.path.dirname(__file__)
 KEYWORDS_FILE = os.path.join(dirname, '../data_transfer/keywords.json')
 CONTENT_FILE = os.path.join(dirname, '../data_transfer/content.json') 
@@ -10,21 +14,23 @@ CONTENT_FILE = os.path.join(dirname, '../data_transfer/content.json')
 newsapi = NewsApiClient(api_key="9d702e3dd4c845fb821fd11840eb3f6a")
 
 def load_keywords():
-    with open(KEYWORDS_FILE, "r") as f:
-        return json.load(f)
+    keywords = []
+    for p in Portfolio.objects.all():
+        keywords.extend([w.strip(string.punctuation) for w in p.assets.split() if w.strip(string.punctuation).isalnum()])
+    return keywords
+
 
 #collect articles with keywords every hours
 def fetch_specific():
 
     content = []
     keywords = load_keywords()      
-    keywords = load_keywords()
-
     now = datetime.now(UTC)
     yesterday = now - timedelta(days=1)  # Fixed: days=1 not day=1
 
     from_date = yesterday.strftime("%Y-%m-%d")
     to_date = now.strftime("%Y-%m-%d")
+    print(keywords)
     for keyword in keywords:
         top_headlines = newsapi.get_everything(q=keyword,
                                                 language='en',
