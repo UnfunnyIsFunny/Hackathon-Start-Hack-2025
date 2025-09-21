5c0adbc98f2f11b9fb2b539e963cfa667955a2bd
Patric Schweizer <patric-richard@bluewin.ch>
2025-09-21 07:01:47 +0200

    Update models.py

--- financeWebApp/interface/models.py
+++ financeWebApp/interface/models.py
@@ -9,17 +9,24 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)  
     verdict = models.CharField(max_length=100, blank=True, null=True)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Portfolio(models.Model):
     name = models.CharField(max_length=100, unique=True)
     assets = models.TextField(help_text="Comma-separated list of assets, e.g. Bitcoin, USD, Gold")
-
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    
     def __str__(self):
         return self.name
     
 class Customer(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    portfolio = models.TextField(help_text="Comma-separated list of portfolio names")
+    portfolio = models.ManyToManyField(Portfolio, blank=True)
     
 class Filing(models.Model):
     company = models.CharField(max_length=200)
