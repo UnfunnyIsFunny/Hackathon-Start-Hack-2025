ff17a50bd38afa3263c19b8337f2b977f9e9fc12
Patric Schweizer <patric-richard@bluewin.ch>
2025-09-21 07:02:05 +0200

    Update admin.py

--- financeWebApp/interface/admin.py
+++ financeWebApp/interface/admin.py
@@ -1,2 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Portfolio, Category
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'assets')
+    list_filter = ('category',)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname')
+    filter_horizontal = ('portfolio',)
